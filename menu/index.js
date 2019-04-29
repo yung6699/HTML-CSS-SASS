4d3519e500cb4c24faf54111f7e8356814023dd6
Yoon <yty3592@gmail.com>
2019-04-29 21:20:22 +0900

    밑에서 올라오는 메뉴 만들기

---
+++ menu/index.js
@@ -0,0 +1,14 @@
+var menu = document.querySelector( "#menu" );
+var sidebar = document.querySelector( "#side-menu" );
+var overlay = document.querySelector( "#overlay" );
+
+  menu.addEventListener( "click", function(){
+  this.classList.toggle( "active" );
+  sidebar.classList.toggle( "active" );
+  overlay.classList.toggle( "active" );
+  overlay.onclick = function () {
+    menu.classList.toggle( "active" );
+    sidebar.classList.toggle( "active" );
+    overlay.classList.toggle( "active" );
+  }
+});
